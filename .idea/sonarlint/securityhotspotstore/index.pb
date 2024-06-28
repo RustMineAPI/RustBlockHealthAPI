a5c01d73f70e4938b1554dd3d87425e9db5babf9
bkkuc01 <bkkuc@bk.ru>
2024-06-28 07:09:44 +0500

    update

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+l
+<src/main/java/pl/bkkuc/rustblockhealth/api/BlockManager.java,7\5\75f4d68daa73f218bae80d0a42a203574526fdc5
+q
+Asrc/main/java/pl/bkkuc/rustblockhealth/api/BlockHealthPlugin.java,b\5\b54ff6eb3c33c6adfdf1e08c52578252ca50b7fb
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
--- src/main/java/pl/bkkuc/rustblockhealth/api/BlockHealthPlugin.java
+++ src/main/java/pl/bkkuc/rustblockhealth/api/BlockHealthPlugin.java
@@ -1,7 +1,16 @@
 package pl.bkkuc.rustblockhealth.api;
 
+import org.bukkit.Material;
+
+import java.util.Map;
+
 public interface BlockHealthPlugin {
 
     BlockManager getBlockManager();
-    
+
+    /**
+     * {@code key} - Материал который может нанести урон.
+     * {@code value} - Урон который наносит.
+     */
+    Map<Material, Integer> getItems();
 }
